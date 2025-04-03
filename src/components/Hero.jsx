03366feed480ef4b3e4000af94524104143433f9
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2025-04-03 10:15:54 -0700

    intial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Hero = () => {
+	return (
+		<div className="h-screen opacity-100 flex flex-col gap-10 items-left justify-center pl-15">
+				<h1 className="text-white text-5xl font-bold">
+					Make in <br />
+					your journey
+				</h1>
+        <p className="text-white"  >Explore the world with what you love beautiful <br /> natural beauty.</p>
+      <div>
+
+      </div>
+		</div>
+	);
+};
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { BsCart, BsCart3, BsMoonFill, BsSunFill } from "react-icons/bs";
+import { FaBarsStaggered } from "react-icons/fa6";
+import logo1 from "../assets/logo1.png";
+import NavLinks from "./NavLinks";
+const Navbar = () => {
+	return (
+		<nav className="bg-[#120B0B82] opacity-100">
+			<div className="px-10">
+				<div className="flex justify-between items-center">
+					{/* TITLE */}
+					<NavLink to="/" className="">
+						<img className="" src={logo1} alt="logo" />
+					</NavLink>
+                    <div className="dropdown ml-6">
+						<label tabIndex={0} className="btn btn-ghost lg:hidden">
+							<FaBarsStaggered className="h-6 w-6" />
+						</label>
+						<ul
+							tabIndex={0}
+							className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-23"
+						>
+							<NavLinks />
+						</ul>
+					</div>
+					<div className="hidden lg:flex">
+						<ul className="menu md:gap-4 xl:gap-10 lg:gap-4 menu-horizontal">
+							<NavLinks />
+						</ul>
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
