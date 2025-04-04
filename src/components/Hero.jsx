a73d7cf4b35cc5d86cc0393f7657af91377b35ff
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2025-04-04 00:58:09 -0700

    add footer

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,37 +1,25 @@
 import React from "react";
-
-const Hero = () => {
+import { Link } from "react-router-dom";
+const Hero = ({ className, children, title, link }) => {
+	if (link === "home" || title === "destination") return <>{children}</>;
 	return (
-		<div className="h-screen opacity-100 flex flex-col gap-10 items-left justify-center pl-20">
-			<h1 className="text-white text-5xl font-bold">
-				Make in <br />
-				your journey
+		<div
+			className={`h-screen opacity-100 flex flex-col gap-6 ${className} justify-center pl-20`}
+		>
+			<h1 className="text-white text-5xl font-bold capitalize">
+				{title}
 			</h1>
-			<p className="text-white opacity-80">
-				Explore the world with what you love beautiful <br /> natural
-				beauty.
-			</p>
-			<div className="bg-white w-lg h-12 flex rounded-4xl px-3 justify-between items-center">
-				<select className="">
-					<option>Location</option>
-				</select>
+			<div className="text-md text-white breadcrumbs">
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
 
-				<select defaultValue="Pick a date" className="">
-					<option>Date</option>
-				</select>
-				<select defaultValue="Pick a color" className="">
-					<option>Date</option>
-				</select>
-				<button className="bg-black text-white rounded-2xl py-1 px-4">
-					Explore now
-				</button>
+					<li>
+						<Link to={`/${link}`}>{link}</Link>
+					</li>
+				</ul>
 			</div>
-			<p className="text-white">
-				Popular Place :{" "}
-				<span className="text-[#CFCFCF]">
-					Bali, Istanbul, Rome, Paris.
-				</span>{" "}
-			</p>
 		</div>
 	);
 };
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
-import { Header, Navbar } from '../components'
+import React from "react";
 
+import { Footer, Header, Hero, Navbar } from "../components";
+import header from "../assets/header.png";
+import { Outlet } from "react-router-dom";
 const Landing = () => {
-  return (
-   <Header/>
-  )
-}
+	return (
+		<>
+			<section
+				className="bg-cover bg-center  h-screen"
+				style={{ backgroundImage: `url(${header})` }}
+			>
+				
 
-export default Landing
+				<Hero link="home">
+					<div className="h-screen opacity-100 flex flex-col gap-6 items-left justify-center pl-20">
+						<h1 className="text-white text-5xl font-bold">
+							Make in <br />
+							your journey
+						</h1>
+						<p className="text-white opacity-80">
+							Explore the world with what you love beautiful{" "}
+							<br /> natural beauty.
+						</p>
+						<div className="bg-white w-lg h-12 flex rounded-4xl px-3 justify-between items-center">
+							<select className="">
+								<option>Location</option>
+							</select>
+
+							<select defaultValue="Pick a date" className="">
+								<option>Date</option>
+							</select>
+							<select defaultValue="Pick a color" className="">
+								<option>Date</option>
+							</select>
+							<button className="bg-black text-white rounded-2xl py-1 px-4">
+								Explore now
+							</button>
+						</div>
+						<p className="text-white">
+							Popular Place :{" "}
+							<span className="text-[#CFCFCF]">
+								Bali, Istanbul, Rome, Paris.
+							</span>{" "}
+						</p>
+					</div>
+				</Hero>
+				<Footer/>
+			</section>
+		</>
+	);
+};
+
+export default Landing;
