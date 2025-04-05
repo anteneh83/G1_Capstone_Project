76de1392a7978be160b5993968c7080ea06157b8
Mesud Ahmed <124754623+Mesud-Ahmed@users.noreply.github.com>
2025-04-05 06:53:40 +0000

    import assests from the design file

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
+import AboutUs from "./AboutUs";
 function App() {
   return (
-    <div className="align-center flex h-dvh bg-blue-200 text-3xl font-bold text-blue-600">
-      <p className="m-auto w-fit"> Hello World</p>
+    <div>
+      <AboutUs />
     </div>
   );
 }
