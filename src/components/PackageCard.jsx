a4f3d1d1a10eeb57e8245e8204f14025c610972f
aposte7 <olman4jesus@gmail.com>
2025-04-06 10:47:50 +0300

    feat: add package cards and images for popular destinations

---
+++ src/components/PackageCard.jsx
@@ -0,0 +1,30 @@
+function PackageCard({ package: offeredPackage }) {
+	return (
+		<div className="space-y-4 w-[27.8rem] mb-6 bg-white shadow-lg rounded-[10px] pb-2">
+			<img
+				src={offeredPackage.imgUrl}
+				className="w-full h-[319px] rounded-t-[10px] bg-stone-500"
+				alt=""
+			/>
+			<div className="px-6 space-y-4 pb-5">
+				<div className="flex justify-between">
+					<p className="font-semibold text-2xl">
+						{offeredPackage.cityName}
+					</p>
+					<p className="text-2xl">{offeredPackage.price}</p>
+				</div>
+				<p className="text-[14px] text-[#555555] font-medium">
+					{offeredPackage.description}
+				</p>
+				<div className="flex justify-between">
+					<div className="text-[1.4rem]">⭐⭐⭐⭐⭐</div>
+					<div className="bg-black font-semibold text-[1.2rem] w-fit h-fit text-white rounded-[30px] py-2 px-4">
+						Discover more
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default PackageCard
--- src/pages/Package.jsx
+++ src/pages/Package.jsx
@@ -1,5 +1,72 @@
-import header from "../assets/package-hero.png";
-import { Navbar,Footer,Hero } from "../components";
+import taiwanImage from '../assets/taiwan.png'
+import thailandImage from '../assets/thailand.png'
+import singaporeImage from '../assets/singapore.png'
+import parisImage from '../assets/paris.png'
+import indonesiImage from '../assets/indonesi.png'
+import swissImage from '../assets/swiss.png'
+
+import header from '../assets/package-hero.png'
+import { Navbar, Footer, Hero } from '../components'
+import PackageCard from '../components/PackageCard'
+
+// Define the packages array outside the component
+const packages = [
+	{
+		imgUrl: parisImage,
+		cityName: 'Paris',
+		price: '$299.00/2days',
+		description: `Lorem ipsum dolor sit amet, 
+            consect adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore
+            incididunt ut labore et dolore`,
+	},
+	{
+		imgUrl: swissImage,
+		cityName: 'Swiss',
+		price: '$299.00/2days',
+		description: `Lorem ipsum dolor sit amet, 
+            consect adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore
+            incididunt ut labore et dolore`,
+	},
+	{
+		imgUrl: thailandImage,
+		cityName: 'Thailand',
+		price: '$299.00/3days',
+		description: `Lorem ipsum dolor sit amet, 
+            consect adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore
+            incididunt ut labore et dolore`,
+	},
+	{
+		imgUrl: taiwanImage,
+		cityName: 'Taiwan',
+		price: '$299.00/3days',
+		description: `Lorem ipsum dolor sit amet, 
+            consect adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore
+            incididunt ut labore et dolore`,
+	},
+	{
+		imgUrl: indonesiImage,
+		cityName: 'Indonesi',
+		price: '$299.00/3days',
+		description: `Lorem ipsum dolor sit amet, 
+            consect adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore
+            incididunt ut labore et dolore`,
+	},
+	{
+		imgUrl: singaporeImage,
+		cityName: 'Singapore',
+		price: '$299.00/3days',
+		description: `Lorem ipsum dolor sit amet, 
+            consect adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore
+            incididunt ut labore et dolore`,
+	},
+]
+
 const Package = () => {
 	return (
 		<>
@@ -13,11 +80,35 @@ const Package = () => {
 					link={`package`}
 				/>
 			</section>
-			<main>
-				<Footer />
-			</main>
-		</>
-	);
-};
+			<main className="bg-white px-14 py-32">
+				<section>
+					<div className="flex items-center justify-between">
+						<div className="flex gap-4 flex-col py-14">
+							<h2 className="font-semibold text-6xl">
+								Popular Destination
+							</h2>
+							<p className="w-[35rem] text-[#878787]">
+								Lorem ipsum dolor sit amet, consectetur
+								adipiscing elit, sed do eiusmod tempor
+								incididunt ut labore et dolore magna
+							</p>
+						</div>
 
-export default Package;
+						<div className="bg-black w-fit h-fit text-white rounded-3xl py-2 px-4">
+							Discover more
+						</div>
+					</div>
+
+					<div className="flex flex-wrap gap-6 items-center py-14">
+						{packages.map((pkg, index) => (
+							<PackageCard key={index} package={pkg} />
+						))}
+					</div>
+				</section>
+			</main>
+			<Footer />
+		</>
+	)
+}
+
+export default Package
