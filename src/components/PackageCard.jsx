b14383a254f526a76e233fa6d162c73acdf84aa7
aposte7 <olman4jesus@gmail.com>
2025-04-11 09:01:02 +0300

    fix: resolve broken responsiveness on Package page and PackageCard component

--- src/components/PackageCard.jsx
+++ src/components/PackageCard.jsx
@@ -1,9 +1,11 @@
+import Button from './Button'
+
 function PackageCard({ package: offeredPackage }) {
 	return (
-		<div className="space-y-4 max-w-[1/3] mb-6 bg-white shadow-lg rounded-[10px] pb-2">
+		<div className="space-y-4  max-w-[27.8rem] min-w-[13rem] mb-6 bg-white shadow-lg rounded-[10px] pb-2">
 			<img
 				src={offeredPackage.imgUrl}
-				className="w-full h-[200px] rounded-t-[10px] bg-stone-500"
+				className="w-full h-[319px] object-cover rounded-t-[10px] bg-stone-500"
 				alt=""
 			/>
 			<div className="px-6 space-y-4 pb-5">
@@ -13,14 +15,14 @@ function PackageCard({ package: offeredPackage }) {
 					</p>
 					<p className="text-2xl">{offeredPackage.price}</p>
 				</div>
-				<p className="text-[14px] text-[#555555] font-medium">
+				<p className="text-[15px] text-[#555555] font-medium">
 					{offeredPackage.description}
 				</p>
-				<div className="flex justify-between">
-					<div className="text-[1.4rem]">⭐⭐⭐⭐⭐</div>
-					<div className="bg-black font-semibold text-[1.2rem] w-fit h-fit text-white rounded-[30px] py-2 px-4">
-						Discover more
+				<div className="flex justify-between  items-center">
+					<div className="xl:text-[1.1rem] xlm:text-[1.3rem]">
+						⭐⭐⭐⭐⭐
 					</div>
+					<Button btnType="medium">Booking Now</Button>
 				</div>
 			</div>
 		</div>
